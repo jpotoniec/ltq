9437828aa87ebdf2869949f3c8a94cf4b443ecd1
Jędrzej 'smaug' Potoniec <smaug@grota.be>
2016-06-21 22:50:17 +0200

    First attempt on benchmarking

---
+++ benchmark.py
@@ -0,0 +1,116 @@
+import gzip
+from pprint import pprint
+from SparqlGraph import SparqlGraph
+from rdflib import URIRef
+import random
+from Engine import Engine
+import itertools
+import json
+import time
+
+dbpedia = SparqlGraph('https://semantic.cs.put.poznan.pl/blazegraph/sparql')
+
+
+def load_queries():
+    with gzip.open('selected_queries.txt.gz', 'rt') as f:
+        for n, line in enumerate(f):
+            if n % 2 == 1:
+                yield line.strip()
+
+
+def evaluate(graph, query, target):
+    target = set(target)
+    result = set([row['uri'] for row in graph.select(query)])
+    missing = target - result
+    unexpected = result - target
+    tp = len(result & target)
+    fp = len(unexpected)
+    prec = tp / (tp + fp)
+    recall = tp / len(target)
+    return {'prec': prec, 'recall': recall, 'f1': 2 / (1 / prec + 1 / recall), 'missing': list(missing),
+            'unexpected': list(unexpected)}
+
+
+def benchmark(q):
+    target = []
+    for row in dbpedia.select(q):
+        assert len(row.values()) == 1
+        uri = list(row.values())[0]
+        if isinstance(uri, URIRef):
+            target.append(uri)
+    target = list(set(target))
+    if len(target) == 0:
+        return None
+    positive = random.sample(target, 5)
+    negative = set(
+        [URIRef('http://dbpedia.org/resource/Bydgoszcz'),
+         URIRef('http://dbpedia.org/resource/Murmur_%28record_label%29'),
+         URIRef('http://dbpedia.org/resource/Julius_Caesar'),
+         URIRef('http://dbpedia.org/ontology/abstract'),
+         URIRef('http://dbpedia.org/property/after'),
+         URIRef('http://dbpedia.org/ontology/Agent'),
+         ])
+    negative = negative - set(target)
+    assert len(negative) > 0
+    eng = Engine(dbpedia, positive, list(negative))
+    log = []
+    for i in range(0, 10):
+        n_steps = 0
+        n_pos = 0
+        n_neg = 0
+        start = time.time()
+        while not eng.hypothesis_good_enough():
+            eng.step()
+            n_steps += 1
+            labels = []
+            n_pos += len(eng.ex_positive)
+            n_neg += len(eng.ex_negative)
+            for ex in itertools.chain(eng.ex_positive, eng.ex_negative):
+                labels.append(ex['uri'] in target)
+            eng.label_examples(labels[:len(eng.ex_positive)], labels[len(eng.ex_positive):])
+        end = time.time()
+        print("Starting evaluation")
+        e = evaluate(dbpedia, eng.final_query(), target)
+        log_line = {'eval': e,
+                    'runtime': {'steps': n_steps, 'requests_p': n_pos, 'requests_n': n_neg},
+                    'query': eng.final_query(),
+                    'perf': {'start': start, 'end': end}}
+        print(e)
+        missing = list(e['missing'])
+        ue = list(e['unexpected'])
+        if len(missing) > 0:
+            if len(missing) > 5:
+                missing = random.sample(missing, 5)
+            eng.positive |= set(missing)
+            log_line['added_positive'] = list(missing)
+        if len(ue) > 0:
+            if len(ue) > 5:
+                ue = random.sample(ue, 5)
+            eng.negative |= set(ue)
+            log_line['added_negative'] = list(missing)
+        log.append(log_line)
+        if len(ue) == 0 and len(missing) == 0:
+            break
+    return log
+
+
+def main():
+    random.seed(12345)
+    queries = load_queries()
+
+    for q in queries:
+        print(q)
+        data = {'query': q}
+        try:
+            log = benchmark(q)
+            data['log'] = log
+        except Exception as e:
+            data['exception'] = str(e)
+        with open('log.json', 'at') as f:
+            print(json.dumps(data), file=f)
+        print("=============================================")
+        # return
+
+
+if __name__ == '__main__':
+    main()
---
+++ queries.py
@@ -0,0 +1,82 @@
+import gzip
+from rdflib import Graph, URIRef
+import re
+from SparqlGraph import SparqlGraph
+import urllib
+from urllib.parse import unquote_plus
+from random import Random
+
+r_select = re.compile(r'select\s+\?S+\s+where', re.I)
+r_var = re.compile(r'(?:\?|\$)\w+', re.I)
+dbpedia = SparqlGraph('https://semantic.cs.put.poznan.pl/blazegraph/sparql')
+
+
+def process_query(q, output):
+    q = unquote_plus(q)
+    if 'select' not in q.lower():
+        return False
+    m = r_select.search(q)
+    if m is None:
+        vars = set(r_var.findall(q))
+        if len(vars) == 0:
+            return False
+        if len(vars) > 1:
+            return False
+    try:
+        # result = dbpedia.select(q)
+        # result = list(result)
+        # print(len(result), file=output)
+        print(q, file=output)
+        return True
+    except urllib.error.HTTPError as e:
+        return False
+
+
+def extract_queries():
+    g = Graph()
+    r = re.compile(r'^\s*sp:text "(.*)"\s*.\s*$')
+    with gzip.open('queries.txt.gz', 'wt') as output:
+        with gzip.open('LSQ-DBpedia351.ttl.gz', 'rt') as f:
+            for n, line in enumerate(f):
+                # if n > 10000:
+                #     return
+                if n % 1000 == 0:
+                    print(n)
+                m = r.match(line)
+                if m is not None:
+                    q = m.group(1)
+                    process_query(q, output)
+
+
+def select_queries():
+    with gzip.open('queries.txt.gz', 'rt') as f:
+        queries = [line.strip() for line in f]
+    r = Random(0xbeef)
+    r.shuffle(queries)
+    n = 0
+    with gzip.open('selected_queries.txt.gz', 'wt') as f:
+        for q in queries:
+            try:
+                result = set()
+                for row in dbpedia.select(q):
+                    if len(row.values()) != 1:
+                        continue
+                    uri = list(row.values())[0]
+                    if isinstance(uri, URIRef):
+                        result.add(uri)
+                result = list(result)
+                if len(result) >= 20:
+                    print(len(result), file=f)
+                    print(q, file=f)
+                    n += 1
+                    print(n)
+                    if n >= 5:
+                        break
+            except urllib.error.HTTPError as e:
+                pass
+
+
+if __name__ == '__main__':
+    select_queries()
+
+# for data from http://aksw.github.io/LSQ/
